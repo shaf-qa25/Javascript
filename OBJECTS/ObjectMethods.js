9f751edd73e180a54c02732ca34d3f75eaa2aba3
Shafqa Fatma <fatmashafqa4@gmail.com>
2025-11-19 17:18:21 +0530

    Object methods

---
+++ OBJECTS/ObjectMethods.js
@@ -0,0 +1,18 @@
+// Assign methods
+
+const person = {
+    firstName: "Shafqa",
+    LastName: "Fatma",
+
+}
+console.log(person);
+
+const xyz={
+    age: 22,
+    email: "shafqa@gmail.com",
+    LastName: "Khan"
+}
+
+Object.assign(person,xyz);
+// target,source ye method h
+console.log(person);
--- OBJECTS/objects1.js
+++ OBJECTS/objects1.js
@@ -6,17 +6,23 @@
 // constructor 
 // const person1 = new Object();
 
-const person = {
-    firstName : "Shafqa",
-    LastName : "Fatma",
-    Age : 20,
-    mobile: [12345678 ,12346659, 1324354657],
-    student : {
-        firstName: "Ajay",
-    }
-};
+// const person = {
+//     firstName : "Shafqa",
+//     LastName : "Fatma",
+//     Age : 20,
+//     // fullName: function(){
+//     //     return this.firstName+" "+this.LastName;
+//     // },
+//     // mobile: [12345678 ,12346659, 1324354657],
+//     // student : {
+//     //     firstName: "Ajay",
+//     // }
+// };
 
-console.log(person);
+// console.log(person);
+
+
+
 
 //ADD PROPERTIES
 
@@ -24,6 +30,9 @@ console.log(person);
 // person['mobile'] = 1234567889;
 
 // console.log(person);
+
+
+
 
 
 //ACCESS PROPERTIES
@@ -34,11 +43,95 @@ console.log(person);
 // console.log(person[abc]);
 
 
+
+
+
+
 //Display Objects in HTML
 
-document.write(person.firstName);
-document.write(person['LastName']);
-let abc= "Age";
-document.write(person[abc]);
-const str = JSON.stringify(person);
-document.write(str);
+// document.write(person.firstName);
+// document.write(person['LastName']);
+// let abc= "Age";
+// document.write(person[abc]);
+// const str = JSON.stringify(person);
+// document.write(str);
+
+// console.log(person.mobile[1]);
+
+// for(const x in person){
+//     console.log(person[x]);
+    
+// }
+
+
+
+
+
+// ADD METHODS 
+
+//  person.fullName = function(){
+//     return this.firstName+" "+this.LastName;
+// };
+
+// person.fullName = king
+// function king(){
+//     return this.firstName+" "+this.LastName;
+// };
+
+// console.log(person.fullName());
+
+
+
+
+// DELETE
+
+// delete person.firstName;
+// console.log(person);
+
+
+
+
+//OBJECTS CONSTRUCTOR METHODS
+// function Person(fname, Lname , em , age){
+//     this.firstName = fname;
+//     this.lastName = Lname;
+//     this.email = em;
+//     this.age = age;
+// }
+
+// const person = new Person('Sachin', 'Tendulkar', 'sachin@gmail.com', 22);
+
+// console.log(person);
+
+// Person.prototype.mobile = 12344577;
+
+
+// //Prototype ek hidden object hota hai jisse tumhara object extra properties inherit karta hai.ko inherit krte h hain constructor mein
+
+
+// console.log(person);
+
+// Person.prototype.fullName = function(){
+//     return this.firstName+' '+this.lastName;
+// };
+
+// console.log(person);
+
+// function A(){
+//     this.x = 1;
+//     return {y:2};
+// };
+// //this object ignored
+// const a = new A();
+// console.log(a);
+// console.log(a.x);
+
+
+// function A(){
+//     this.x = 1;
+//     return 2;
+// };
+// //primitive ignored
+// const a = new A();
+// console.log(a);
+// console.log(a.x);
